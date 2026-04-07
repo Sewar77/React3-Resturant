4669cec23c6de6669302295db818df07471f1a1c
Sewar77 <sewarjihad793@gmail.com>
2026-04-07 11:42:18 +0300

    create test component

--- resturant/src/App.jsx
+++ resturant/src/App.jsx
@@ -1,8 +1,11 @@
+import Test from "./components/Test";
+
 function App() {
   return (
     <>
       <h1>Hi, I AM SEWAR</h1>
       <p>this is my new react projetc</p>
+      <Test />
       <img src="../public/favicon.svg" alt="" />
       <p>this is image</p>
     </>
---
+++ resturant/src/components/Test.jsx
@@ -0,0 +1,10 @@
+function Test() {
+  return (
+    <>
+      <h1>this is text</h1>
+      <p>this is test test test</p>
+    </>
+  );
+}
+
+export default Test;
