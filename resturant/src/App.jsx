320dbbb9d100c111f7262616e403d70982b621e3
Sewar77 <sewarjihad793@gmail.com>
2026-04-12 11:54:53 +0300

    feat: add menu component with card layout and styling with MUI

--- resturant/src/App.jsx
+++ resturant/src/App.jsx
@@ -1,9 +1,11 @@
 import About from "./components/About/About.jsx";
+import Menu from "./components/Menu/Menu.jsx";
 
 function App() {
   return (
     <>
       <About />
+      <Menu />
     </>
   );
 }
--- resturant/src/components/Menu/Menu.jsx
+++ resturant/src/components/Menu/Menu.jsx
@@ -1,8 +1,127 @@
-function Menu(){
-return <>
-
-
-
-</>
+import {
+  Card,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material";
+function Menu() {
+  return (
+    <>
+      <Container>
+        <Typography variant="h1">Menu</Typography>
+        <Grid
+          container
+          spacing={4}
+          sx={{
+            padding: 5,
+            margin: 5,
+          }}
+        >
+          <Grid size={4}>
+            <Card
+              sx={{
+                padding: 5,
+                textAlign: "center",
+              }}
+            >
+              <CardMedia
+                image="../../../src/assets/card.jpg"
+                title="Image"
+                sx={{
+                  height: 100,
+                }}
+              />
+              <CardContent>
+                <Typography variant="h3">Menu1</Typography>
+                <Typography variant="body1">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                  earum natus modi consequatur cupiditate amet a sequi quam
+                  ducimus deleniti repudiandae eveniet nemo quis totam ipsa sint
+                  minima, veniam delectus.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={4}>
+            <Card
+              sx={{
+                padding: 5,
+                textAlign: "center",
+              }}
+            >
+              <CardMedia
+                image="../../../src/assets/card.jpg"
+                title="Image"
+                sx={{
+                  height: 100,
+                }}
+              />
+              <CardContent>
+                <Typography variant="h3">Menu1</Typography>
+                <Typography variant="body1">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                  earum natus modi consequatur cupiditate amet a sequi quam
+                  ducimus deleniti repudiandae eveniet nemo quis totam ipsa sint
+                  minima, veniam delectus.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={4}>
+            <Card
+              sx={{
+                padding: 5,
+                textAlign: "center",
+              }}
+            >
+              <CardMedia
+                image="../../../src/assets/card.jpg"
+                title="Image"
+                sx={{
+                  height: 100,
+                }}
+              />
+              <CardContent>
+                <Typography variant="h3">Menu1</Typography>
+                <Typography variant="body1">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                  earum natus modi consequatur cupiditate amet a sequi quam
+                  ducimus deleniti repudiandae eveniet nemo quis totam ipsa sint
+                  minima, veniam delectus.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid size={4}>
+            <Card
+              sx={{
+                padding: 5,
+                textAlign: "center",
+              }}
+            >
+              <CardMedia
+                image="../../../src/assets/card.jpg"
+                title="Image"
+                sx={{
+                  height: 100,
+                }}
+              />
+              <CardContent>
+                <Typography variant="h3">Menu1</Typography>
+                <Typography variant="body1">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos
+                  earum natus modi consequatur cupiditate amet a sequi quam
+                  ducimus deleniti repudiandae eveniet nemo quis totam ipsa sint
+                  minima, veniam delectus.
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
-export default Menu
+export default Menu;
